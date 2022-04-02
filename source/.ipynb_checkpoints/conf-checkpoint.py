35bebd71362890c8f5d7b03d7ed97ee7e7bd917d
Frank Cichos <cichos@physik.uni-leipzig.de>
2022-04-02 09:22:26 +0200

    corrected path for binder

--- source/.ipynb_checkpoints/conf-checkpoint.py
+++ source/.ipynb_checkpoints/conf-checkpoint.py
@@ -117,7 +117,7 @@ nbsphinx_prolog = """
 
     .. nbinfo::
         This page was generated from `{{ docname }}`.
-        :raw-html:`<br/><a href="https://mybinder.org/v2/gh/fcichos/CompSoft22/main?urlpath=tree/{{ docname }}"><img alt="Binder badge" src="https://img.shields.io/badge/launch-full%20binder-red.svg" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<br/><a href="https://mybinder.org/v2/gh/fcichos/CompSoft22/main?urlpath=tree/source/{{ docname }}"><img alt="Binder badge" src="https://img.shields.io/badge/launch-full%20binder-red.svg" style="vertical-align:text-bottom"></a>`
 
 .. only:: latex
 
